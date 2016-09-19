591f7fb934e70082490d787962b73c598a3f3271
Axel Ronquist <aronquist@gmail.com>
2016-09-19 12:17:28 +0200

    Fixed indentation

--- Ch 01. Arrays and Strings/1.Is Unique/1. Is_unique.c
+++ Ch 01. Arrays and Strings/1.Is Unique/1. Is_unique.c
@@ -6,31 +6,29 @@
 
 bool isUniqueChars(char *str,int size)
 {
-if(size > 128)
-	return false;
-	
-bool index[128] = {0};
-int i;
-for (i = 0; i < size; i++)
-{
-    int val = str[i];
-    if(index[val])
-    {
+    if(size > 128)
         return false;
+	
+    bool index[128] = {0};
+    int i;
+    for (i = 0; i < size; i++)
+    {
+        int val = str[i];
+        if(index[val])
+            return false;
+        index[val] = true;
+    } 
+    return true;
     }
-    index[val] = true;
-} 
-return true;
-}
 
 int main()
 {
-char str[] = "CTCI";
-int size = strlen(str);
-if (isUniqueChars(str,size)) 
-	printf("Unique");
-else
-    printf("Not unique");
-getchar();
-return 0;
+    char str[] = "CTCI";
+    int size = strlen(str);
+    if (isUniqueChars(str,size)) 
+        printf("Unique");
+    else
+        printf("Not unique");
+    getchar();
+    return 0;
 }
